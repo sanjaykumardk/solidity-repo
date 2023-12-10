f2a14c4ac62e051cd991e0a7f386bd26f15efa66
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2023-12-10 18:14:31 +0530

    Update calldata-memory-storage.sol

--- calldata-memory-storage.sol
+++ calldata-memory-storage.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+//To add values like an array :
 
 contract SimpleStorage
 {
+    struct People
+    {
+        uint favouriteNumber;
+        string name;
+    }
+    People[] public people;
+
     function addPerson(string memory, _name, uint _favouriteNUmber) public
     {
-        people.push(People( _favouriteNumber, _name));
+        people.push(People(_name,_favouriteNumber));
     }
 }
 // calldata,memory - temporary storage
