0cff218fcf8ce733ba3f8d59af732ec9e5072862
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2023-12-15 18:14:00 +0530

    Update import-contract.sol

--- import-contract.sol
+++ import-contract.sol
@@ -1,21 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.0;
+
 import "./SimpleStorage.sol";
-contract StorageFactory
-{
+
+contract StorageFactory {
     SimpleStorage[] public simplestoragearray;
-    function CreateSimpleStorageContract() public 
-    {
-        //here simplestorage has been created
+
+    function createSimpleStorageContract() public {
+        // Create a new instance of SimpleStorage
         SimpleStorage simplestorage = new SimpleStorage();
+        
+        // Add the new instance to the array
         simplestoragearray.push(simplestorage);
     }
-}
-//For Single Contract
-/*
-SimpleStorage public simplestorage;
-    function CreateSimpleStorageContract() public 
-    {
-      simplestorage = new SimpleStorage();
+
+    // For Single Contract
+    /*
+    SimpleStorage public simplestorage;
+    function createSimpleStorageContract() public {
+        simplestorage = new SimpleStorage();
     }
-*/
+    */
+
+    function SFstorage(uint256 simplestorageindex, uint256 simplestoragenumber) public {
+        // Retrieve the SimpleStorage instance from the array
+        SimpleStorage simplestorage = simplestoragearray[simplestorageindex];
+        
+        // Call the Store function on the retrieved SimpleStorage instance
+        simplestorage.Store(simplestoragenumber);
+    }
+
+    function SFget(uint256 simplestorageindex) public view returns (uint256) {
+        // Retrieve the SimpleStorage instance from the array
+        SimpleStorage simplestorage = simplestoragearray[simplestorageindex];
+        
+        // Call the retrieve function on the retrieved SimpleStorage instance and return the result
+        return simplestorage.retreive();
+    }
+}
+
