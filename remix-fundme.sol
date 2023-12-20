64236870056c880425ac1986bad5db39dc50aa27
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2023-12-20 22:56:05 +0530

    Create remix-fundme.sol

---
+++ remix-fundme.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract FundMe
+{
+    uint256 public Number;
+
+    function fund()public payable
+    {
+        //payable changes our orange fund to red fund
+        //we can value attribute using : 
+        Number = 10;
+        require(msg.value > 1e18 , "Didn't send enough !!!"); // Atleast one eth or polygon or avalanche 
+        // require is like a if-else condition 
+        // msg.value is true condition otherwise it will revert the error as "Didn't send enough !!!"
+
+        //1e18 == 1 * 10 ** 18 == 1000000000000000000 wei
+        // that is 1 x 10 power 18 weis
+    }
+
+    //function withdraw{}
+}
