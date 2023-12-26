23c115066ccc489010560c01dd7a835f7e06d871
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2023-12-26 10:05:37 +0530

    Update fundme.sol

--- fundme.sol
+++ fundme.sol
@@ -15,26 +15,56 @@ data from external computation data into our blockchain=
  
  //Chain Link Data Feeds !!!!!!!!!!!!!!!!!!
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+
 
 interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
 
-  function description() external view returns (string memory);
+  function decimals()
+    external
+    view
+    returns (
+      uint8
+    );
 
-  function version() external view returns (uint256);
+  function description()
+    external
+    view
+    returns (
+      string memory
+    );
+
+  function version()
+    external
+    view
+    returns (
+      uint256
+    );
 
   function getRoundData(
     uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+  )
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
 
   function latestRoundData()
     external
     view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
 
+}
 
 contract FundMe
 {
@@ -48,9 +78,19 @@ contract FundMe
 
     //function withdraw{}
 
-    function getPrice()public view returns(uint256)
+    function getPrice()public 
     {
-        AggregatorV3Interface pricefeed = AggregatorV3Interface(0xa9001779A20dAFaAD5aB463dE917D9a0e6Da7EaA);
-        return pricefeed.version();
+        //Adress : 0xa9001779A20dAFaAD5aB463dE917D9a0e6Da7EaA
+    }
+
+    function getVersion() public view returns(uint256)
+    {
+      AggregatorV3Interface pricefeed = AggregatorV3Interface(0xa9001779A20dAFaAD5aB463dE917D9a0e6Da7EaA);
+      return pricefeed.version();
+    }
+
+    function getConversionRate() public 
+    {
+
     }
 }
